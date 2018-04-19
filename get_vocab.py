75d3e7c7c9a57245789fbb0f2433ff66650226b9
Ethan Mines <elmines325@gmail.com>
2018-04-19 17:03:41 -0500

    WIP: Make get_vocab.py callable from other Python scripts

--- get_vocab.py
+++ get_vocab.py
@@ -5,9 +5,27 @@ from collections import Counter
 
 c = Counter()
 
-for line in sys.stdin:
-    for word in line.split():
-        c[word] += 1
+def write_vocab(instream, outstream):
 
-for key,f in sorted(c.items(), key=lambda x: x[1], reverse=True):
-    print(key+" "+ str(f))
+    if instream = sys.stdin:
+        for line in sys.stdin:
+            for word in line.split():
+                c[word] += 1
+    else:
+        with open(instream, "r", encoding="utf-8") as instr:
+            for line in instr.readlines():
+                for word in line.split():
+                    c[word] += 1
+
+    if outstream = sys.stdout:
+        for key,f in sorted(c.items(), key=lambda x: x[1], reverse=True):
+            print(key+" "+ str(f))
+    else:
+        with open(outstream, "w", encoding="utf-8") as out:
+            for key,f in sorted(c.items(), key=lambda x: x[1], reverse=True):
+                out.write( key + " " + str(f) + "\n" )
+
+if __name__ == "__main__":
+    instream = sys.stdin if len(sys.argv) == 0 else sys.argv[0]
+    oustream = sys.stdout if len(sys.argv) < 2 else sys.argv[1]
+    write_vocab(instream, outstream)
